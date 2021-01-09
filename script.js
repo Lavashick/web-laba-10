660e5fccc5a7f91d93138ec3d0270178aa716167
Lavashick <lavashick@icloud.com>
2021-01-09 04:30:49 +0300

    Проверка на победителя

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let canvas = document.getElementById("c1")
 let ctx = canvas.getContext("2d")
 
+let firstOrSecondPlayer = document.getElementById("first-or-second")
+let XorO = document.getElementById("x-or-y")
+let firstPlayerCount = document.getElementById("first-player-result")
+let secondPlayerCount = document.getElementById("second-player-result")
+let firstPlayerCountNum = 0
+let secondPlayerCountNum = 0
+let win = document.getElementById("win")
 
 function createCanvasLines() {
     ctx.strokeStyle = "#353a2d"
@@ -97,6 +104,40 @@ function calculatePosition(x, y) {
     return [Math.floor(x / 100), Math.floor(y / 100)]
 }
 
+// Изменение матрицы зависимо поставленных фигур
+function changeMatrPoint(point) {
+    let chetClick = countClick % 2
+    if ((matr[point[0]][point[1]]) == 2) {
+        matr[point[0]][point[1]] = chetClick
+        countClick += 1
+        drawXorO(point, chetClick)
+        resultFinal = checkFinal();
+        if (resultFinal == 2) {
+            console.log("НИЧЬЯ!")
+            firstPlayerCountNum += 1
+            secondPlayerCountNum += 1
+            firstPlayerCount.innerHTML = String(firstPlayerCountNum)
+            secondPlayerCount.innerHTML = String(secondPlayerCountNum)
+            printWinner(resultFinal)
+            clearCanvas()
+        }
+        if (resultFinal == 0) {
+            console.log("ВЫИГРАЛ НОЛИК!")
+            firstPlayerCountNum += 1
+            firstPlayerCount.innerHTML = String(firstPlayerCountNum)
+            printWinner(resultFinal)
+            clearCanvas()
+        }
+        if (resultFinal == 1) {
+            console.log("ВЫИГРАЛ КРЕСТИК!")
+            secondPlayerCountNum += 1
+            secondPlayerCount.innerHTML = String(secondPlayerCountNum)
+            printWinner(resultFinal)
+            clearCanvas()
+        }
+    }
+}
+
 
 // Отрисовка крестика или нолика на canvas
 function drawXorO(point, chetClick) {
@@ -108,6 +149,71 @@ function drawXorO(point, chetClick) {
         drawX(point[0] * 100, point[1] * 100)
         firstOrSecondPlayer.innerHTML = "1"
         XorO.innerHTML = "O"
+    }
+}
+
+// Проверяет, есть ли победитель
+function checkFinal() {
+    if (countClick != 9) {
+        // По главной диагонали
+        if ((matr[0][0] == matr[1][1]) && (matr[0][0] == matr[2][2])) {
+            let winner = matr[0][0]
+            if (winner != 2) {
+                return winner
+            }
+        }
+        // По побочной диагонали
+        if ((matr[0][2] == matr[1][1]) && (matr[1][1] == matr[2][0])) {
+            let winner1 = matr[1][1]
+            if (winner1 != 2) {
+                return winner1
+            }
+        }
+        // Вертикально 0
+        if ((matr[0][0] == matr[0][1]) && (matr[0][1] == matr[0][2])) {
+            let winner2 = matr[0][1]
+            if (winner2 != 2) {
+                return winner2
+            }
+        }
+        // Вертикально 1
+        if ((matr[1][0] == matr[1][1]) && (matr[1][1] == matr[1][2])) {
+            let winner3 = matr[1][1]
+            if (winner3 != 2) {
+                return winner3
+            }
+        }
+        // Вертикально 2
+        if ((matr[2][0] == matr[2][1]) && (matr[2][1] == matr[2][2])) {
+            let winner4 = matr[2][1]
+            if (winner4 != 2) {
+                return winner4
+            }
+        }
+
+        // Горизонтально 0
+        if ((matr[0][0] == matr[1][0]) && (matr[1][0] == matr[2][0])) {
+            let winner5 = matr[1][0]
+            if (winner5 != 2) {
+                return winner5
+            }
+        }
+        // Горизонтально 1
+        if ((matr[0][1] == matr[1][1]) && (matr[1][1] == matr[2][1])) {
+            let winner6 = matr[1][1]
+            if (winner6 != 2) {
+                return winner6
+            }
+        }
+        // Горизонтально 2
+        if ((matr[0][2] == matr[1][2]) && (matr[1][2] == matr[2][2])) {
+            let winner7 = matr[1][2]
+            if (winner7 != 2) {
+                return winner7
+            }
+        }
+    } else {
+        return 2
     }
 }
 
